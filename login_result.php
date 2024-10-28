520ffa22e43ce29eac225fd6dd20d952ad55eb5b
askapeggy <aska.aska@msa.hinet.net>
2024-10-28 14:35:20 +0800

    新增 登入程式

---
+++ login_result.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>回應登入頁面</title>
+</head>
+<body>
+    <?php
+        $a1 = 'aska';
+        $p1 = 'aska';
+        if(empty($_POST['account']))
+        {
+            echo "請輸入帳號<br>";
+            echo "<a href='login.php'>回到登入頁面</a>";
+            exit();
+        }else
+        {
+            $a = $_POST['account'];
+        }
+        if(empty($_POST['password']))
+        {
+            echo "請輸入密碼<br>";
+            echo "<a href='login.php'>回到登入頁面</a>";
+            exit();
+        }else
+        {
+            $p = $_POST['password'];
+        }
+        if($a != $a1)
+        {
+            echo "查無此帳號<br>";
+            echo "<a href='login.php'>回到登入頁面</a>";
+            exit();
+        }
+        if($p != $p1)
+        {
+            echo "密碼錯誤<br>";
+            echo "<a href='login.php'>回到登入頁面</a>";
+            exit();
+        }
+        echo "<h1>登入成功</h1>";
+        echo "<a href='login.php?r='OK'>回到登入頁面</a>";
+?>
+</body>
+</html>

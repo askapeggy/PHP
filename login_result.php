7e09e38452cf5538e06e75788ad2b634cb3cb424
askapeggy <aska.aska@msa.hinet.net>
2024-10-28 16:02:39 +0800

    更新

--- login_result.php
+++ login_result.php
@@ -9,7 +9,11 @@
     <?php
         $a1 = 'aska';
         $p1 = 'aska';
+<<<<<<< HEAD
         if(!isset($_POST['account']))
+=======
+        if(isset($_POST['account']))
+>>>>>>> fde1955 (萬年曆)
         {
             echo "請輸入帳號<br>";
             echo "<a href='login.php'>回到登入頁面</a>";

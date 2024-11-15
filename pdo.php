5c47823298db924b8bc40aae12d064abf5f43de4
askapeggy <aska.aska@msa.hinet.net>
2024-11-15 08:19:42 +0800

    上課內容

---
+++ pdo.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDO</title>
+    <style>
+        table {
+            border-collapse: collapse; /* 合併邊框 */
+            width: 50%; /* 表格寬度 */
+            height: 50%; /* 自動調整高度 */
+            background-color: rgba(128, 128, 128, 0.5); /* 表格背景顏色 */
+            border: 2px solid black; /* 設置單元格的邊框，1px 寬，黑色 */
+            border-radius: 10px;
+            padding: 0px;
+            margin: auto;
+
+        }
+        table tr:nth-child(1) td
+        {
+            background-color: #cc6;
+            color:darkorage;
+            text-shadow:2px 2px 2px #fff
+        }
+        td {
+            border: 1px solid black; /* 設置單元格的邊框，1px 寬，黑色 */
+            padding: 8px; /* 增加單元格內邊距 */
+            text-align: center; /* 文字居中 */
+        }
+    </style>
+</head>
+<body>
+    <h1>資料庫連線</h1>
+    <?php
+        $dsn = "mysql:host=localhost;charset=utf8;dbname=school;";
+        $pdo = new PDO($dsn, 'root', '');
+        $sql = "select * from classes";
+        $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        //echo $row['id']."-".$row['name'].$row['tutor']."<br>";
+        echo "<table>";
+        echo "<tr>";
+        echo "<td>序號</td>";
+        echo "<td>班級名稱</td>";
+        echo "<td>導師</td>";
+        echo "</tr>";
+        foreach($rows as $row)
+        {
+            echo "<tr>";
+
+            echo "<td>";
+            echo $row['id'];
+            echo "</td>";
+
+            echo "<td>";
+            echo "<a href=classes_detail.php?code=".$row['code'].">";
+            echo $row['name'];
+            echo "</a>";
+            echo "</td>";
+
+            echo "<td>";
+            echo $row['tutor'];
+            echo "</td>";
+
+            echo "</tr>";
+        }
+        echo "</table>";
+        echo "<pre>";
+        print_r($rows);
+        echo "</pre>";
+        $pdo = null;
+    ?>
+</body>
+</html>
